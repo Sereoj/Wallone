11441c25d378f8e3572c3ca4bfe39f6e5e8e59f9
Sergey Egorov <ser.12egorov@gmail.com>
2022-01-04 15:32:42 +0300

    Помощник исправления относительных ссылок

---
+++ WinDynamicDesktop.Core/Helpers/UriHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinDynamicDesktop.Core.Helpers
+{
+    public class UriHelper
+    {
+        /**
+         * /public/catalog/hello
+         * to 
+         * http://v3.w2me.ru/public/catalog/hello
+         */
+        public static Uri Get(string path)
+        {
+            Uri uri = new Uri(path);
+            return uri.IsAbsoluteUri ? uri : new Uri("http://v3.w2me.ru" + path);
+        }
+
+        public static Uri Get(Uri path)
+        {
+            return path.IsAbsoluteUri ? path : new Uri("http://v3.w2me.ru" + path);
+        }
+    }
+}
