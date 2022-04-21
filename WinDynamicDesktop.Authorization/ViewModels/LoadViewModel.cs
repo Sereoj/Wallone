fed16c866aef481d4bd040884137c9914ea5354b
Sergey Egorov <ser.12egorov@gmail.com>
2022-04-21 15:01:17 +0300

    Фикс загрузки

--- WinDynamicDesktop.Authorization/ViewModels/LoadViewModel.cs
+++ WinDynamicDesktop.Authorization/ViewModels/LoadViewModel.cs
@@ -178,6 +178,7 @@ namespace WinDynamicDesktop.Authorization.ViewModels
                 }
                 else
                 {
+                    IsLoading = false;
                     SetMessage("Нет соединения c " + Router.OnlyNameDomainApi());
                 }
 
