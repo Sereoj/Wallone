17453511e50b00f2b69da0127b584790d97c2826
Sergey Egorov <ser.12egorov@gmail.com>
2022-04-23 14:40:57 +0300

    Исправление отображения списка категории

--- Wallone.Core/Models/Brand.cs
+++ Wallone.Core/Models/Brand.cs
@@ -6,6 +6,6 @@
         public string Name { get; set; }
         public string Icon { get; set; }
         public string Tag { get; set; }
-        public string Status { get; set; }
+        public bool Status { get; set; }
     }
 }
