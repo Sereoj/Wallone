e4bcd4f01ebe1adaebc649087529794902801b8d
Sergey Egorov <ser.12egorov@gmail.com>
2022-10-06 01:42:43 +0300

    Изменение поиска и высоты

--- src/Wallone.UI/ViewModels/Wallpapers/WallpapersViewModel.cs
+++ src/Wallone.UI/ViewModels/Wallpapers/WallpapersViewModel.cs
@@ -2,20 +2,6 @@
 using Prism.Mvvm;
 using Prism.Regions;
 using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Media.Imaging;
-using Wallone.Core.Builders;
-using Wallone.Core.Helpers;
-using Wallone.Core.Interfaces;
-using Wallone.Core.Models;
-using Wallone.Core.Services.Pages;
-using Wallone.UI.Interfaces;
-using Wallone.UI.Services;
-using Wallone.UI.ViewModels.Controls;
-
 namespace Wallone.UI.ViewModels.Wallpapers
 {
     public class WallpapersViewModel : BindableBase
